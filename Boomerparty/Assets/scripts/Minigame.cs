589bdf63d5c157b4f1e459416e0ef07fb7f48e5e
Tomdatbenik <tomvankaathoven@gmail.com>
2020-05-14 13:35:32 +0200

    Website jatter

---
+++ Boomerparty/Assets/scripts/Minigame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minigame
+{
+    public string title;
+    public string smallDescription;
+    public string imgUrl;
+    public string url;
+
+    public List<string> explanationSteps;
+
+    public Minigame(string title, string smallDescription, string imgUrl, string url, List<string> explanationSteps)
+    {
+        this.title = title;
+        this.smallDescription = smallDescription;
+        this.imgUrl = imgUrl;
+        this.url = url;
+        this.explanationSteps = explanationSteps;
+    }
+}
---
+++ Boomerparty/Assets/scripts/WebsiteScraper.cs
@@ -0,0 +1,62 @@
+﻿using HtmlAgilityPack;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using UnityEngine;
+
+public class WebsiteScraper : MonoBehaviour
+{
+    public List<Minigame> minigamesList = new List<Minigame>();
+
+    private void Start()
+    {
+        using (WebClient client = new WebClient())
+        {
+            string htmlCode = client.DownloadString("https://samenspelen.online/spellen-overzicht/");
+            HtmlDocument mainDocument = new HtmlDocument();
+
+            mainDocument.LoadHtml(htmlCode);
+
+            List<HtmlNode> minigames = mainDocument.DocumentNode.Descendants("div").Where(node => node.HasClass("item")).ToList();
+
+            foreach (HtmlNode minigameNode in minigames)
+            {
+                HtmlNode titleNode = minigameNode.Descendants("a").Where(n => n.HasClass("title_link")).First();
+                HtmlNode smalldescriptionNode = minigameNode.Descendants("div").Where(n => n.HasClass("excerpt")).First();
+                HtmlNode imgurlNode = minigameNode.Descendants("img").First();
+
+                string title = HttpUtility.HtmlDecode(titleNode.InnerText);
+                string smallDescription = HttpUtility.HtmlDecode(titleNode.InnerText);
+                string url = titleNode.Attributes["href"].Value;
+                string imgUrl = imgurlNode.Attributes["data-src"].Value;
+
+
+                string htmlCodeExplenation = client.DownloadString(url);
+                HtmlDocument explanationDocument = new HtmlDocument();
+                explanationDocument.LoadHtml(htmlCodeExplenation);
+
+                HtmlNode explanation = explanationDocument.DocumentNode.Descendants("ol").First();
+
+                List<string> steps = new List<string>();
+
+                foreach (HtmlNode step in explanation.Descendants("span").ToList())
+                {
+                    steps.Add(step.InnerText);
+                }
+
+                minigamesList.Add(new Minigame(title, smallDescription, imgUrl, url, steps));
+            }
+        }
+
+        Minigame minigame = minigamesList.First();
+        
+        Debug.Log("title: " + minigame.title + " description: " + minigame.smallDescription);
+
+        foreach(string step in minigame.explanationSteps)
+        {
+            Debug.Log(step);
+        }
+    }
+}
